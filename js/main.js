939b6b9d382b4ed0e6550b1335238e493e0308d0
Toby Flemming <tobyf93@hotmail.com>
2015-05-11 17:04:34 +1000

    Using 2D array for modelling move data.

--- js/main.js
+++ js/main.js
@@ -20,26 +20,41 @@ app.registerEvents = function() {
 	$('#board').on('click', '.cell', this.makeMove);
 };
 
-// Create a 2D array for moves.
+// Create a blank 2D array for moves.
 app.createBoardArray = function() {
-	$('.cell').each(function() {
-		
+	$('.row').each(function() {
+		var row = [];
+
+		$('.cell', this).each(function() {
+			row.push('');
+		});
+
+		app.board.push(row);
 	});
 };
 
 app.makeMove = function() {
 	$this = $(this);
+	var row = $this.parent().attr('data-row');
+	var col = $this.attr('data-col');
 
 	// Don't allow moves to be overwritten
-	if ($this.html() !== '') {
+	if (app.board[row][col] !== '') {
 		return;
 	}
 
+	// Update View
 	$(this).html(app.currentMove);
+
+	// Update Board (model)
+	app.updateBoard(row, col, app.currentMove);
+	
 	app.currentMove = (app.currentMove === 'X') ? 'O' : 'X';
 };
 
-
+app.updateBoard = function(row, col, move) {
+	this.board[row][col] = move;
+};
 
 
 // Begin
